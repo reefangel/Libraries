c543e8eca2d277e3a2477fdd84f674f5c5c720ee
Roberto Imai <roberto@imlogo.com>
2012-05-28 18:33:17 -0700

    issue #2: Removed Piezo references

--- Globals/Globals.h
+++ Globals/Globals.h
@@ -159,7 +159,8 @@ const prog_char NoIMCheck1[] PROGMEM = "Found";
 #define VPin                0
 #define HPin                1
 #define PHPin               6
-#define Piezo               16
+// issue #2 - Piezo Not needed anymore
+//#define Piezo               16 
 
 //Digital I/O
 #define ledPin              7
