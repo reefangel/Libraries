3d02a768cf6b45a43d5bd8ea87d35f4ff18d34f3
Jens Horn <jenshorn@gmail.com>
2012-08-29 08:24:01 +0200

    Added inline function UseFlexiblePhCalibration
    Added inline function to trigger feature

--- ReefAngel/ReefAngel.h
+++ ReefAngel/ReefAngel.h
@@ -167,6 +167,7 @@ public:
 	void inline AddDateTimeMenu() {};
 	void inline AddRFExpansion() {};
 	void inline AddCustomColors() {};
+	void inline UseFlexiblePhCalibration() {};
 	void StandardLights(byte LightsRelay, byte OnHour, byte OnMinute, byte OffHour, byte OffMinute);
 	void MHLights(byte LightsRelay, byte OnHour, byte OnMinute, byte OffHour, byte OffMinute, byte MHDelay);
 	void StandardHeater(byte HeaterRelay, int LowTemp, int HighTemp);
