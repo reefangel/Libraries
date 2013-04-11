20900ed4f5c765284480a512721da5fc4550945a
Lee Nevo <lnevo@usa.net>
2013-04-11 06:45:41 -0400

    Added support for Override of RelayMasks

--- ReefAngel/ReefAngel.h
+++ ReefAngel/ReefAngel.h
@@ -137,11 +137,17 @@ public:
 	byte FeedingModePorts;
 	byte WaterChangePorts;
 	byte OverheatShutoffPorts;
+#ifdef OVERRIDE_PORTS
+  byte OverridePorts;
+#endif
 #ifdef RelayExp
 	// Expansion Module ports
 	byte FeedingModePortsE[MAX_RELAY_EXPANSION_MODULES];
 	byte WaterChangePortsE[MAX_RELAY_EXPANSION_MODULES];
 	byte OverheatShutoffPortsE[MAX_RELAY_EXPANSION_MODULES];
+#ifdef OVERRIDE_PORTS
+  byte OverridePortsE[MAX_RELAY_EXPANSION_MODULES];
+#endif
 #endif  // RelayExp
 #ifndef RemoveAllLights
 	byte LightsOnPorts;
@@ -167,6 +173,7 @@ public:
 	void inline AddDateTimeMenu() {};
 	void inline AddRFExpansion() {};
 	void inline AddCustomColors() {};
+	void inline AddPortOverrides() {};
 	void inline Display24h() {};
 	void inline UseFlexiblePhCalibration() {};
 	void inline ReverseATOLow() {};
