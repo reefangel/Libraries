987ae422b3b18ed7926ea6f6d500601cf9143784
Lee Nevo <lnevo@usa.net>
2013-05-03 16:48:07 -0400

    This file is required for DOxygen home page. It is only a .h file with the description of the main project

---
+++ mainpage.h
@@ -0,0 +1,17 @@
+/*! \mainpage 
+ * Reef Angel is the first open-source controller to offer the reefing hobbyist 
+ * full control of hardware and software to accommodate their reefing needs.  
+ * At your fingertips is the ability to create the most sophisticated controller 
+ * you will ever need. With a very simple to use interface and easy to learn programming 
+ * language, Reef Angel will give you endless possibilities.  Reef Angel was built in 
+ * the same open-source electronics prototyping platform as the world famous 
+ * <A HREF=http://arduino.cc/>Arduino.</A>
+ * What does it mean? It means you can rest assured that your reef controller is working 
+ * on the most stable platform on the market.  Programmers from all over the world have 
+ * spent countless hours perfecting the coding on this platform.  Not familiar with the 
+ * programming world? Just looking for an all-in-one and out-of-the-box solution for 
+ * your reef system? Not a problem. Reef Angel Controller comes with its own built-in 
+ * demo software that will leave you with your mouth open as it's taking care of your 
+ * reef system. More details? <A HREF=http://www.reefangel.com>Click here.</A>  
+ *
+ */
