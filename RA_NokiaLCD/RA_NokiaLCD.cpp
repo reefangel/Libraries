1b8ea6bd1bcb953072b2fb4717ab3a2732d132b6
Roberto Imai <roberto@imlogo.com>
2013-09-06 10:13:00 -0700

    Nokia DrawImage fix for Gen3 screens

--- RA_NokiaLCD/RA_NokiaLCD.cpp
+++ RA_NokiaLCD/RA_NokiaLCD.cpp
@@ -1477,7 +1477,15 @@ void RA_NokiaLCD::DrawImage(int swidth, int sheight, byte x, byte y, const prog_
         for (byte j = 0; j < 30; j++)
         {
             count+=1;
-            if (count<=swidth*sheight) SendData(~pgm_read_byte_near(iPtr++));
+            if (count<=swidth*sheight)
+            	if (LCDID==0)
+            	{
+            		SendColor12Bit(pgm_read_byte_near(iPtr++));
+            	}
+            	else
+            	{
+            		SendData(~pgm_read_byte_near(iPtr++));
+            	}
         }
     }
     while (count < swidth*sheight);
