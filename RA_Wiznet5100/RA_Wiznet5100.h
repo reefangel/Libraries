a28de3d57c319a69332062aba4ad51c8d6faf751
Roberto Imai <roberto@imlogo.com>
2016-05-07 16:28:50 -0700

    Updated the MQTT server ip address

--- RA_Wiznet5100/RA_Wiznet5100.h
+++ RA_Wiznet5100/RA_Wiznet5100.h
@@ -25,7 +25,7 @@ static byte NetMac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 //static byte MQTTServer[] = { 212, 72, 74, 21 }; // broker.mqttdashboard.com
 //static byte MQTTServer[] = { 10, 1, 10, 130 }; // local test
 //static byte MQTTServer[] = { 107, 22, 157, 224 }; // m11.cloudmqtt.com
-static byte MQTTServer[] = { 69, 198, 171, 165 }; // forum.reefangel.com
+static byte MQTTServer[] = { 23, 24, 193, 9 }; // forum.reefangel.com
 static IPAddress NetIP(192,168,1,200);
 //const IPAddress PortalServer(198,171,134,6); // www
 //const IPAddress PortalServer(192,168,9,140); // local
